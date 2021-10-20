bba19c4c8c4eac16c5c02a2ac0bb53ebb67a38b5
Prosper Evergreen <prosper.evergreen@tuni.fi>
2021-10-20 17:33:18 +0300

    setup default role programmatically & refactor role setup

--- backend/app/config/functions/roleSetup.js
+++ backend/app/config/functions/roleSetup.js
@@ -1,25 +1,14 @@
-const ADMIN = 'Admin';
-const EMPLOYEE = 'Employee';
-
 const findRoleByName = (name) => {
-  return strapi
-    .query("role", "users-permissions")
-    .findOne({name});
+  return strapi.query("role", "users-permissions").findOne({ name });
 };
 
 /**
- * 
- * @param {string} name role name
- * @param {string} description description
+ *
+ * @param {{name: string; description: string; type: string}} role role object
  * @returns Promise that resolves into
  */
-const createRole = (name, description) => {
-  return strapi.query("role", "users-permissions")
-    .create({
-      name,
-      description,
-      type: name.toLowerCase()
-    });
+const createRole = (role) => {
+  return strapi.query("role", "users-permissions").create(role);
 };
 
 /**
@@ -28,29 +17,72 @@ const createRole = (name, description) => {
  */
 const enableApplicationPermissions = async (role) => {
   const permissionQuery = strapi.query("permission", "users-permissions");
-  const applicationPermissions = await permissionQuery.find({ type: 'application', role});
-  await Promise.all(applicationPermissions.map(({id})=> permissionQuery.update({id}, {enabled:true})));
+  const applicationPermissions = await permissionQuery.find({
+    type: "application",
+    role,
+  });
+  await Promise.all(
+    applicationPermissions.map(({ id }) =>
+      permissionQuery.update({ id }, { enabled: true })
+    )
+  );
 };
 
 /**
  * create admin and employee roles if they are not yet created
  * enable all application permissions to them
+ *
+ * @param {Array<{name: string; description: string, type: string}>} roles roles array to be created
  */
-const roleSetup = async () => {
-  let adminRole = await findRoleByName(ADMIN);
-  let employeeRole = await findRoleByName(EMPLOYEE);
-  if (!adminRole) adminRole= await createRole(ADMIN, 'Admin user');
-  if (!employeeRole) employeeRole = await createRole(EMPLOYEE, 'Employee user');
-  await enableApplicationPermissions(adminRole.id);
-  await enableApplicationPermissions(employeeRole.id);
+const roleSetup = async (roles) => {
+  await roles.map(async (role) => {
+    let customeRole = await findRoleByName(role.name);
+    if (!customeRole) customeRole = await createRole(role);
+    await enableApplicationPermissions(customeRole.id);
+  });
   // no role provided will enable application permissions to every role, uncomment this to apply
   // await enableApplicationPermissions();
+};
+
+/**
+ * A function that sets the default role for new users
+ *
+ * @param {{name: string; description: string, type: string}} default_role Default role object
+ * @returns
+ */
+const defaultRoleSetup = async (default_role) => {
+  // Try to get role
+  const role = await strapi
+    .query("role", "users-permissions")
+    .findOne({ name: default_role.name });
+
+  // Check if role exists
+  if (!role) return;
+
+  const pluginStore = await strapi.store({
+    environment: "",
+    type: "plugin",
+    name: "users-permissions",
+  });
+
+  // Get the app settings configuration
+  const settings = await pluginStore.get({
+    key: "advanced",
+  });
+
+  // Check if role has been already assigned as default type
+  if (role.type === settings.default_role) return;
+
+  // Set the default role to the role type
+  await pluginStore.set({
+    key: "advanced",
+    value: { ...settings, default_role: role.type },
+  });
 };
 
 module.exports = {
   findRoleByName,
   createRole,
   roleSetup,
-  ADMIN,
-  EMPLOYEE,
+  defaultRoleSetup,
 };
