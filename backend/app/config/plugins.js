850a8f169ca55c424752e132927a99c2f4e12aac
Jori Väinölä <jori.vainola@tuni.fi>
2021-10-20 21:39:54 +0300

    feat: AWS SES implementation

--- backend/app/config/plugins.js
+++ backend/app/config/plugins.js
@@ -1,18 +1,30 @@
 module.exports = ({ env }) => ({
+  // email: {
+  //   provider: 'nodemailer',
+  //   providerOptions: {
+  //     host: env('SMTP_HOST'),
+  //     port: env('SMTP_PORT'),
+  //     auth: {
+  //       user: env('SMTP_USERNAME'),
+  //       pass: env('SMTP_PASSWORD'),
+  //     },
+  //     // ... any custom nodemailer options
+  //   },
+  //   settings: {
+  //     defaultFrom: env('EMAIL_FROM'),
+  //     defaultReplyTo: env('EMAIL_REPLY_TO'),
+  //   },
+  // },
   email: {
-    provider: 'nodemailer',
+    provider: 'amazon-ses',
     providerOptions: {
-      host: env('SMTP_HOST'),
-      port: env('SMTP_PORT'),
-      auth: {
-        user: env('SMTP_USERNAME'),
-        pass: env('SMTP_PASSWORD'),
-      },
-      // ... any custom nodemailer options
+      key: 'AKIAU4VVH26Y5BS5B2HX',
+      secret: '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',
+      amazon: `https://email.us-east-2.amazonaws.com`
     },
     settings: {
-      defaultFrom: env('EMAIL_FROM'),
-      defaultReplyTo: env('EMAIL_REPLY_TO'),
-    },
+      defaultFrom: 'clavuyep@gmail.com',
+      defaultReplyTo: 'clavuyep@gmail.com'
+    }
   },
 });
