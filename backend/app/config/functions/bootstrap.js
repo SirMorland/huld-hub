fdd7f7fbba340751aaaf53729a3d44547d494f40
Prosper Evergreen <prosper.evergreen@tuni.fi>
2021-10-20 17:34:35 +0300

    update bootstrap and refactor setup user function

--- backend/app/config/functions/bootstrap.js
+++ backend/app/config/functions/bootstrap.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,16 +10,45 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-const { roleSetup } = require('./roleSetup');
-const { userSetup } = require('./userSetup');
- 
+const { roleSetup, defaultRoleSetup } = require("./roleSetup");
+const { userSetup } = require("./userSetup");
+
+const DEFAULT_ROLES = [
+  {
+    name: "Admin",
+    descripton: "Admin user",
+    type: "admin",
+  },
+  {
+    name: "Employee",
+    descripton: "Employee user",
+    type: "employee",
+  },
+];
+
+const DEFAULT_USERS = [
+  {
+    username: "huld-admin",
+    email: "huld-admin@huld.io",
+    password: "huld-admin",
+    role: "Admin" // Must match a role name 
+  },
+  {
+    username: "huld-employee",
+    email: "huld-employee@huld.io",
+    password: "huld-employee",
+    role: "Employee" // Must match a role name
+  },
+];
+
 module.exports = async () => {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     try {
-      await roleSetup();
-      await userSetup();
+      await roleSetup(DEFAULT_ROLES);
+      await userSetup(DEFAULT_USERS);
+      await defaultRoleSetup(DEFAULT_ROLES.find((role) => role.name === "Employee"));
     } catch (e) {
-      console.error('Something went wrong in bootstraping', e);
+      console.error("Something went wrong in bootstraping", e);
     }
   }
 };
