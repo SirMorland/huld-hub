71039b8f070261573dd3d343b786785427a412fb
Jori Väinölä <jori.vainola@tuni.fi>
2021-10-20 22:07:25 +0300

    aws ses plugin commented out

--- backend/app/config/plugins.js
+++ backend/app/config/plugins.js
@@ -1,30 +1,32 @@
 module.exports = ({ env }) => ({
+  // NODEMAILER
+  email: {
+    provider: 'nodemailer',
+    providerOptions: {
+      host: env('SMTP_HOST'),
+      port: env('SMTP_PORT'),
+      auth: {
+        user: env('SMTP_USERNAME'),
+        pass: env('SMTP_PASSWORD'),
+      },
+      // ... any custom nodemailer options
+    },
+    settings: {
+      defaultFrom: env('EMAIL_FROM'),
+      defaultReplyTo: env('EMAIL_REPLY_TO'),
+    },
+  },
+  //AMAZON SIMPLE EMAIL SERVICE
   // email: {
-  //   provider: 'nodemailer',
+  //   provider: 'amazon-ses',
   //   providerOptions: {
-  //     host: env('SMTP_HOST'),
-  //     port: env('SMTP_PORT'),
-  //     auth: {
-  //       user: env('SMTP_USERNAME'),
-  //       pass: env('SMTP_PASSWORD'),
-  //     },
-  //     // ... any custom nodemailer options
+  //     key: env('AWS_ACCESS_KEY_ID'),
+  //     secret: env('AWS_SECRET_ACCESS_KEY'),
+  //     amazon: `https://email.${env('AWS_REGION')}.amazonaws.com`
   //   },
   //   settings: {
   //     defaultFrom: env('EMAIL_FROM'),
-  //     defaultReplyTo: env('EMAIL_REPLY_TO'),
-  //   },
+  //     defaultReplyTo: env('EMAIL_REPLY_TO')
+  //   }
   // },
-  email: {
-    provider: 'amazon-ses',
-    providerOptions: {
-      key: 'AKIAU4VVH26Y5BS5B2HX',
-      secret: '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',
-      amazon: `https://email.us-east-2.amazonaws.com`
-    },
-    settings: {
-      defaultFrom: 'clavuyep@gmail.com',
-      defaultReplyTo: 'clavuyep@gmail.com'
-    }
-  },
 });
