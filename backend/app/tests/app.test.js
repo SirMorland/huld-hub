234bf60754bdcce56cc19c1588c44fff0dae0ca3
Hoang Tran <hoang.tran@tuni.fi>
2021-10-21 19:43:47 +0300

    update unit test

--- backend/app/tests/app.test.js
+++ backend/app/tests/app.test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { setupStrapi } = require('./helpers/strapi');
 
-jest.setTimeout(15000);
+jest.setTimeout(60000);
 
 /** this code is called once before any test is called */
 beforeAll(async () => {
--- backend/app/tests/user-profiles/index.js
+++ backend/app/tests/user-profiles/index.js
@@ -11,14 +11,60 @@ describe("user-profiles api", () => {
   });
   // clean up all competences
   afterAll(async () => {
-    const data = await strapi.query("user-profiles").find();
-    const promises = data.map(({ id }) =>
+    const dataProfile = await strapi.query("user-profiles").find();
+    const promiseProfile = dataProfile.map(({ id }) =>
       strapi.services["user-profiles"].delete({ id })
     );
-    await Promise.all(promises);
+    await Promise.all(promiseProfile);
+
+    const dataCompetences = await strapi.query("competences").find();
+    const promiseCompetences = dataCompetences.map(({ id }) =>
+      strapi.services["competences"].delete({ id })
+    );
+    await Promise.all(promiseCompetences);
+
+    const dataCat = await strapi.query("competence-categories").find();
+    const promiseCat = dataCat.map(({ id }) =>
+      strapi.services["competence-categories"].delete({ id })
+    );
+    await Promise.all(promiseCat);
   });
   //
   it("should return user-profiles with get", async () => {
+    let categoryData = {
+      name: "Skills",
+      description: "Skills",
+    };
+    let category = await strapi.services["competence-categories"].create(
+      categoryData
+    );
+
+    let competenceData = {
+      name: "PHP",
+      description: "PHP",
+      category: category.id,
+    };
+    const competence = await strapi.services["competences"].create(
+      competenceData
+    );
+
+    const userData = {
+      username: "test_user_profiles1",
+      email: "test_user_profiles1@test.com",
+      provider: "string",
+      password: "123456",
+      resetPasswordToken: "",
+      confirmationToken: "",
+      confirmed: false,
+      blocked: false,
+      role: "public",
+      created_by: 1,
+      updated_by: 1,
+    };
+    const user = await strapi.plugins["users-permissions"].services.user.add({
+      ...userData,
+    });
+
     const profileData = {
       name: "test 1",
       title: "test 1",
@@ -47,7 +93,8 @@ describe("user-profiles api", () => {
           description: "test 1",
         },
       ],
-      user: 1,
+      competences: [competence.id],
+      user: user.id,
       created_by: 1,
       updated_by: 1,
     };
@@ -59,12 +106,50 @@ describe("user-profiles api", () => {
       .then((data) => {
         expect(data.body).toHaveLength(1);
         expect(data.body[0].name).toBe(profileData.name);
-        expect(data.body[0].work_experiences[0].id).toBe(profileData.work_experiences[0].id);
-        expect(data.body[0].education_histories[0].id).toBe(profileData.education_histories[0].id);
+        expect(data.body[0].work_experiences[0].id).toBe(
+          profileData.work_experiences[0].id
+        );
+        expect(data.body[0].education_histories[0].id).toBe(
+          profileData.education_histories[0].id
+        );
       });
   });
   //
   it("should return a user-profile with get", async () => {
+    let categoryData = {
+      name: "Positions",
+      description: "Positions",
+    };
+    let category = await strapi.services["competence-categories"].create(
+      categoryData
+    );
+
+    let competenceData = {
+      name: "Web Dev",
+      description: "Web Dev",
+      category: category.id,
+    };
+    const competence = await strapi.services["competences"].create(
+      competenceData
+    );
+
+    const userData = {
+      username: "test_user_profiles2",
+      email: "test_user_profiles2@test.com",
+      provider: "string",
+      password: "123456",
+      resetPasswordToken: "",
+      confirmationToken: "",
+      confirmed: false,
+      blocked: false,
+      role: "public",
+      created_by: 1,
+      updated_by: 1,
+    };
+    const user = await strapi.plugins["users-permissions"].services.user.add({
+      ...userData,
+    });
+
     const profileData = {
       name: "test 2",
       title: "test 2",
@@ -93,7 +178,8 @@ describe("user-profiles api", () => {
           description: "test 2",
         },
       ],
-      user: 1,
+      competences: [competence.id],
+      user: user.id,
       created_by: 1,
       updated_by: 1,
     };
@@ -105,12 +191,51 @@ describe("user-profiles api", () => {
         expect(data.body.name).toBe(profileData.name);
         expect(data.body.title).toBe(profileData.title);
         expect(data.body.email).toBe(profileData.email);
-        expect(data.body.work_experiences[0].id).toBe(profileData.work_experiences[0].id);
-        expect(data.body.education_histories[0].id).toBe(profileData.education_histories[0].id);
+        expect(data.body.work_experiences[0].id).toBe(
+          profileData.work_experiences[0].id
+        );
+        expect(data.body.education_histories[0].id).toBe(
+          profileData.education_histories[0].id
+        );
       });
   });
+
   //
   it("should be able to add a user-profile", async () => {
+    let categoryData = {
+      name: "Positions",
+      description: "Positions",
+    };
+    let category = await strapi.services["competence-categories"].create(
+      categoryData
+    );
+
+    let competenceData = {
+      name: "Web Dev",
+      description: "Web Dev",
+      category: category.id,
+    };
+    const competence = await strapi.services["competences"].create(
+      competenceData
+    );
+
+    const userData = {
+      username: "test_user_profiles3",
+      email: "test_user_profiles3@test.com",
+      provider: "string",
+      password: "123456",
+      resetPasswordToken: "",
+      confirmationToken: "",
+      confirmed: false,
+      blocked: false,
+      role: "public",
+      created_by: 1,
+      updated_by: 1,
+    };
+    const user = await strapi.plugins["users-permissions"].services.user.add({
+      ...userData,
+    });
+
     const profileData = {
       name: "test 3",
       title: "test 3",
@@ -139,7 +264,8 @@ describe("user-profiles api", () => {
           description: "test 3",
         },
       ],
-      user: 1,
+      competences: [competence.id],
+      user: user.id,
       created_by: 1,
       updated_by: 1,
     };
@@ -151,8 +277,12 @@ describe("user-profiles api", () => {
         expect(data.body.name).toBe(profileData.name);
         expect(data.body.title).toBe(profileData.title);
         expect(data.body.email).toBe(profileData.email);
-        expect(data.body.work_experiences[0].id).toBe(profileData.work_experiences[0].id);
-        expect(data.body.education_histories[0].id).toBe(profileData.education_histories[0].id);
+        expect(data.body.work_experiences[0].id).toBe(
+          profileData.work_experiences[0].id
+        );
+        expect(data.body.education_histories[0].id).toBe(
+          profileData.education_histories[0].id
+        );
       });
   });
 });
