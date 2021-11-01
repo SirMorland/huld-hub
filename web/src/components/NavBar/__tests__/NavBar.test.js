45632277cc56109f0ba89a5bd5a24fb12043248c
Phu Nguyen <ngp130895@gmail.com>
2021-11-01 21:20:04 +0200

    test: not render admin when logged with employee

--- web/src/components/NavBar/__tests__/NavBar.test.js
+++ web/src/components/NavBar/__tests__/NavBar.test.js
@@ -14,7 +14,10 @@ describe('NavBar', () => {
   it('should render admin when logged in with admin', () => {
     const { getByText } = renderWithTheme(<NavBar loggedIn role="admin" />);
     expect(getByText(/admin/i)).toBeInTheDocument();
-  })
+  it('should not render admin when logged in with employee', () => {
+    const { queryByText } = renderWithTheme(<NavBar loggedIn role="employee" />);
+    expect(queryByText(/admin/i)).not.toBeInTheDocument();
+  });
   it('should run log out function when logout is clicked', () => {
     const onLogOutClick = jest.fn();
     const { getByText } = renderWithTheme(<NavBar loggedIn role="employee" onLogOutClick={onLogOutClick}/>);
