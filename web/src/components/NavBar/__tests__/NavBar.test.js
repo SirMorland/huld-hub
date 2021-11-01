c1212d6e20178c3f2bf0651da2a701b16fe1381d
Phu Nguyen <ngp130895@gmail.com>
2021-11-01 21:20:17 +0200

    chore: format tests

--- web/src/components/NavBar/__tests__/NavBar.test.js
+++ web/src/components/NavBar/__tests__/NavBar.test.js
@@ -6,22 +6,23 @@ describe('NavBar', () => {
   it('should render homepage', () => {
     const { getByText } = renderWithTheme(<NavBar />)
     expect(getByText(/hub/i)).toBeInTheDocument();
-  })
+  });
   it('should render log out side bar when logged in', () => {
-    const { getByText } = renderWithTheme(<NavBar loggedIn role="employee"/>);
+    const { getByText } = renderWithTheme(<NavBar loggedIn role="employee" />);
     expect(getByText(/log out/i)).toBeInTheDocument();
-  })
+  });
   it('should render admin when logged in with admin', () => {
     const { getByText } = renderWithTheme(<NavBar loggedIn role="admin" />);
     expect(getByText(/admin/i)).toBeInTheDocument();
+  });
   it('should not render admin when logged in with employee', () => {
     const { queryByText } = renderWithTheme(<NavBar loggedIn role="employee" />);
     expect(queryByText(/admin/i)).not.toBeInTheDocument();
   });
   it('should run log out function when logout is clicked', () => {
     const onLogOutClick = jest.fn();
-    const { getByText } = renderWithTheme(<NavBar loggedIn role="employee" onLogOutClick={onLogOutClick}/>);
+    const { getByText } = renderWithTheme(<NavBar loggedIn role="employee" onLogOutClick={onLogOutClick} />);
     fireEvent.click(getByText(/log out/i));
     expect(onLogOutClick).toBeCalled();
-  })
+  });
 });
