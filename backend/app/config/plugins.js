dee9d73f2775349485454c9f059c24a68cfb9607
Phu Nguyen <ngp130895@gmail.com>
2021-11-03 20:46:36 +0200

    feat: added strapi s3 plugin for uploads

--- backend/app/config/plugins.js
+++ backend/app/config/plugins.js
@@ -15,4 +15,22 @@ module.exports = ({ env }) => ({
       defaultReplyTo: env('EMAIL_REPLY_TO'),
     },
   },
+  upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env('AWS_KEY'),
+      secretAccessKey: env('AWS_SECRET'),
+      region: env('AWS_REGION', 'eu-west-1'),
+      s3ForcePathStyle: env('AWS_S3_FORCE_PATH_STYLE'),
+      sslEnabled: env('AWS_SSL_ENABLED'),
+      endpoint: env('NODE_ENV') === 'development' ? {
+        host: `localhost:${env('AWS_S3_PORT')}`,
+        hostname: env('AWS_S3_HOSTNAME'),
+        port: env('AWS_S3_PORT'),
+      } : undefined,
+      params: {
+        Bucket: env('AWS_S3_BUCKET'),
+      }
+    },
+  }
 });
