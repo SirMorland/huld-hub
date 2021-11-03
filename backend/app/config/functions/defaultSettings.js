58dfb0de50f8b508fa4a26e6819959d9e0495177
Phu Nguyen <ngp130895@gmail.com>
2021-11-03 19:48:13 +0200

    fix: strapi store didn't update config correctly

--- backend/app/config/functions/defaultSettings.js
+++ backend/app/config/functions/defaultSettings.js
@@ -6,23 +6,20 @@
  */
 const defaultSettings = async (default_settings) => {
   await Promise.all(
-    default_settings.map(async (setting) => {
-      const pluginStore = await strapi.store({
-        ...(setting.environment && { environment: setting.environment }),
-        ...(setting.type && { type: setting.type }),
-        ...(setting.name && { name: setting.name }),
-      });
+    default_settings.map(async ({ environment, type, name, key, value }) => {
+      const params = {
+        environment,
+        type,
+        name,
+        key,
+      };
 
       // Get the app settings configuration
-      const prev_setting = await pluginStore.get({
-        key: setting.key,
-      });
-
+      const prevSettings = await strapi.store(params).get();
       // Set the default role to the role type
-      await pluginStore.set({
-        key: setting.key,
-        value: { ...prev_setting, ...setting.value },
-      });
+      await strapi
+        .store(params)
+        .set({ value: { ...prevSettings, ...value } });
     })
   );
 };
