d22e1677d54494014aa64b8260b0973046505093
Phu Nguyen <ngp130895@gmail.com>
2021-11-12 15:04:07 +0200

    fix: autocomplete button color

--- web/src/components/SelectAutocompleteField.js
+++ web/src/components/SelectAutocompleteField.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import { styled } from '@mui/system';
+
+const StyledAutocomplete = styled(Autocomplete)(({ theme }) => ({
+  button: {
+    color: theme.palette.text.primary
+  }
+}));
+
 
 const SelectAutocompleteField = ({options, label, onSelect, ...props}) => {
   const [random, setRandom] = useState(Math.random());
@@ -10,7 +18,7 @@ const SelectAutocompleteField = ({options, label, onSelect, ...props}) => {
     setRandom(Math.random());
   }
   return (
-    <Autocomplete
+    <StyledAutocomplete
       options={options}
       getOptionLabel={option => option.name}
       data-testid="select-autocomplete-field"
