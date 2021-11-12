cd1c7b040c6c097dcbf4abd73d0cba2052bc1bdf
Phu Nguyen <ngp130895@gmail.com>
2021-11-12 15:11:27 +0200

    fix: corrected label color

--- web/src/components/SelectAutocompleteField.js
+++ web/src/components/SelectAutocompleteField.js
@@ -3,11 +3,11 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { styled } from '@mui/system';
 
-const StyledAutocomplete = styled(Autocomplete)(({ theme }) => ({
-  button: {
-    color: theme.palette.text.primary
+const StyledAutocomplete = styled(Autocomplete)(({ theme }) => (`
+  button, label {
+    color: ${theme.palette.text.primary};
   }
-}));
+`));
 
 
 const SelectAutocompleteField = ({options, label, onSelect, ...props}) => {
