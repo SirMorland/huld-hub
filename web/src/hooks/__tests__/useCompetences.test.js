304f2d323746df636ca940287fa13afa5611092d
Phu Nguyen <ngp130895@gmail.com>
2021-11-13 09:47:31 +0200

    test: fixed test accordingly to useCompetences change

--- web/src/hooks/__tests__/useCompetences.test.js
+++ web/src/hooks/__tests__/useCompetences.test.js
@@ -20,12 +20,13 @@ describe('useCompetenceCategories hook', () => {
     expect(result.current.length).toEqual(0);
   })
   it('should return categories list when hub-jwt is defined and the jwt is proper', async () => {
-    const mockCompetences = [{ id: 1, name: 'cat' }];
-    fetch.mockResponseOnce(JSON.stringify(mockCompetences), { status: 200 });
+    const mockCompetencesResponse = [{ id: 1, name: 'cat', category: { id: 1 } }];
+    const mockCompetencesActual = [{ id: 1, name: 'cat', category: 1 }];
+    fetch.mockResponseOnce(JSON.stringify(mockCompetencesResponse), { status: 200 });
 
-    const { result, waitForNextUpdate } = renderHook(() => useCompetences('cat','jwt'));
+    const { result, waitForNextUpdate } = renderHook(() => useCompetences('cat', 'jwt'));
     await waitForNextUpdate();
-    expect(result.current).toEqual(mockCompetences);
+    expect(result.current).toEqual(mockCompetencesActual);
   });
 
 })
