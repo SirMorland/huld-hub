d962afbeecb8f43ae4717a816dfbd262501feb17
Prosper Evergreen <prosper.evergreen@tuni.fi>
2021-11-15 19:28:26 +0200

    update tests

--- web/src/components/HistoryList/__tests__/HistoryListEdit.test.js
+++ web/src/components/HistoryList/__tests__/HistoryListEdit.test.js
@@ -1,8 +1,5 @@
-import { cleanup, getByTestId } from "@testing-library/react";
-import {
-  renderWithTheme,
-  capitalizeFirstLetters,
-} from "../../../utils";
+import { cleanup, getByTestId, fireEvent } from "@testing-library/react";
+import { renderWithTheme, capitalizeFirstLetters } from "../../../utils";
 import HistoryListEdit from "../HistoryListEdit.js";
 
 const testData = {
@@ -104,7 +101,9 @@ describe("HistoryListEdit component", () => {
       );
       expect(organisationEl).toBeTruthy();
       const organisationInput = getByTestId(organisationEl, "textfield-input");
-      expect(organisationInput.value).toBe(testData.historyItems[index].organisation);
+      expect(organisationInput.value).toBe(
+        testData.historyItems[index].organisation
+      );
       expect(organisationInput).toBeTruthy();
 
       const titleEl = getByTestId(educationHitoryItem, "textfield-title");
@@ -116,16 +115,24 @@ describe("HistoryListEdit component", () => {
       const start_dateEl = getByTestId(educationHitoryItem, "grid-start_date");
       expect(start_dateEl).toBeTruthy();
       const start_dateInput = getByTestId(start_dateEl, "date-input");
-    expect(start_dateInput).toBeTruthy();
-    const startDate = new Date(testData.historyItems[index].start_date)
-    expect(start_dateInput.value).toBe(`${startDate.getDate()}.${startDate.getMonth()+1}.${startDate.getFullYear()}`);
+      expect(start_dateInput).toBeTruthy();
+      const startDate = new Date(testData.historyItems[index].start_date);
+      expect(start_dateInput.value).toBe(
+        `${startDate.getDate()}.${
+          startDate.getMonth() + 1
+        }.${startDate.getFullYear()}`
+      );
 
       const end_dateEl = getByTestId(educationHitoryItem, "grid-end_date");
       expect(end_dateEl).toBeTruthy();
       const end_dateInput = getByTestId(end_dateEl, "date-input");
       expect(end_dateInput).toBeTruthy();
-      const endDate = new Date(testData.historyItems[index].end_date)
-      expect(end_dateInput.value).toBe(`${endDate.getDate()}.${endDate.getMonth()+1}.${endDate.getFullYear()}`);
+      const endDate = new Date(testData.historyItems[index].end_date);
+      expect(end_dateInput.value).toBe(
+        `${endDate.getDate()}.${
+          endDate.getMonth() + 1
+        }.${endDate.getFullYear()}`
+      );
 
       const descriptionEl = getByTestId(
         educationHitoryItem,
@@ -134,7 +141,51 @@ describe("HistoryListEdit component", () => {
       expect(descriptionEl).toBeTruthy();
       const descriptionInput = getByTestId(descriptionEl, "textfield-input");
       expect(descriptionInput).toBeTruthy();
-      expect(descriptionInput.value).toBe(testData.historyItems[index].description);
+      expect(descriptionInput.value).toBe(
+        testData.historyItems[index].description
+      );
     });
+  });
+
+  it("should add new item", () => {
+    const { getByTestId } = renderWithTheme(
+      <HistoryListEdit
+        type={testData.type}
+        historyItems={testData.historyItems}
+      />
+    );
+    const HistoryListEditEl = getByTestId("edit-history-list");
+    expect(HistoryListEditEl).toBeTruthy();
+    expect(HistoryListEditEl.children).toHaveLength(
+      testData.historyItems.length
+    );
+    const addBtn = getByTestId("btn-add-new");
+    fireEvent.click(addBtn);
+    expect(HistoryListEditEl.children).toHaveLength(
+      testData.historyItems.length + 1
+    );
+  });
+
+  it("should remove an item", () => {
+    const constainer = renderWithTheme(
+      <HistoryListEdit
+        type={testData.type}
+        historyItems={testData.historyItems}
+      />
+    );
+    const HistoryListEditEl = constainer.getByTestId("edit-history-list");
+    expect(HistoryListEditEl).toBeTruthy();
+    expect(HistoryListEditEl.children).toHaveLength(
+      testData.historyItems.length
+    );
+    const firstItem = constainer.getAllByTestId("history-edit-item")[0]
+
+    const removeItemBtn = getByTestId(firstItem, "btn-delete")
+
+    fireEvent.click(removeItemBtn);
+    expect(HistoryListEditEl.children).toHaveLength(
+      testData.historyItems.length - 1
+    );
+
   });
 });
