41ce83e73105bf0cb351482a201400c9e3011d17
Ville Helppolainen <ville.helppolainen@gmail.com>
2021-11-16 18:56:57 +0200

    fix: fixed grid layout for profile pages

--- web/src/pages/ProfilePage/ProfilePageEdit.js
+++ web/src/pages/ProfilePage/ProfilePageEdit.js
@@ -65,7 +65,7 @@ const Work = styled("div")`
   }
   @media (min-width: 1152px) {
     grid-column-start: 2;
-    grid-row: span 4;
+    grid-row: span 7;
   }
 `;
 const Education = styled("div")`
@@ -74,7 +74,7 @@ const Education = styled("div")`
   }
   @media (min-width: 1152px) {
     grid-column-start: 3;
-    grid-row: span 4;
+    grid-row: span 7;
   }
 `;
 
