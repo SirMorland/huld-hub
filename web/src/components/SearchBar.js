fd8f93e436373fc9ccaa8c97dd6baf0df79195c3
Hoang Tran <hoang.tran@tuni.fi>
2021-11-17 17:13:10 +0200

    feat: adding SearchBar component

---
+++ web/src/components/SearchBar.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { TextField } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { styled } from "@mui/system";
+
+const Wrapper = styled("div")(`
+    display: flex;
+    gap: 16px;
+`);
+
+const StyledIcon = styled(ArrowForwardIosIcon)(`
+  color: #fff;
+`);
+
+const StyledIconButton = styled(IconButton)(`
+  background-color: #0047F2;
+  border-radius: 0;
+  width: 56px;
+  height: 56px;
+  cursor: pointer;
+`);
+
+const SearchBar = () => {
+  const [state , setState ] = useState();
+  const onInputChange = (event) => {
+    setState(event.target.value);
+  }
+  const onSearch = () => {
+    console.log(state);
+  };
+
+  return (
+    <Wrapper>
+      <TextField onChange={onInputChange} fullWidth id="search_keyword" type="text" label="" name="search_keyword" />
+      <StyledIconButton aria-label="search" onClick={() => { onSearch(); }} >
+        <StyledIcon />
+      </StyledIconButton>
+    </Wrapper>
+  );
+};
+
+SearchBar.propTypes = {};
+
+SearchBar.defaultProps = {};
+
+export default SearchBar;
