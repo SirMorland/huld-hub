ce9b458f8f40ac243185a40891290ee7f7259db1
kgsapo <sai.polineni@tuni.fi>
2021-11-25 18:55:58 +0200

    fix: save and cancel buttons are changed

--- web/src/components/ActionButtonContainer.js
+++ web/src/components/ActionButtonContainer.js
@@ -1,17 +1,24 @@
 import { styled } from '@mui/system';
 
-const StyledContainer = styled('div')`
+const StyledContainer = styled('div') (({ theme }) => `
+  left: 0px;
+  width: 100%;
   position: fixed;
-  bottom: 16px;
+  bottom: 0px;
   display: grid;
   grid-auto-flow: column;
   gap: 32px;
   z-index: 1;
+  justify-content: right;
+  background-color: ${theme.palette.primary.main};
+  padding: 16px;
+
 
   @media (min-width: 768px){
-    bottom: 32px;
+    bottom: 0px;
+    padding: 32px;
   }
-`;
+`);
 
 const ActionButtonContainer = ({ children }) => {
   return (
