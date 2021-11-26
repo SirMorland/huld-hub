dcd99dfc39eda051bacf93c4e401638009cc75ba
Hoang Tran <hoang.tran@tuni.fi>
2021-11-26 13:27:35 +0200

    feat adding admin edit competences list

--- web/src/pages/AdminPage.js
+++ web/src/pages/AdminPage.js
@@ -5,6 +5,10 @@ import { styled } from "@mui/system";
 import Page from "../components/Page/Page";
 import ActionButtonContainer from "../components/ActionButtonContainer";
 import useEmailDomain from "../hooks/useEmailDomai";
+import ItemListEdit from "../components/ItemListEdit";
+import TextField from "../components/TextField";
+import useCompetences from "../hooks/useCompetences";
+import { useUserContext } from "../userContext";
 
 const Admins = styled("div")`
   @media (min-width: 768px) {
@@ -44,20 +48,82 @@ const Keywords = styled("div")`
 function AdminPage() {
   const emailDomain = useEmailDomain();
 
+  const { jwt } = useUserContext();
+  const [languages, setLanguages] = useCompetences("coding languages", jwt);
+  const [keywords, setKeywords] = useCompetences("keywords", jwt);
+  
+  const onLanguageAdd = (newLanguage) => {
+    setLanguages({
+      ...languages,
+      newLanguage,
+    });
+  };
+
+  const onKeywordAdd = (newKeyword) => {
+    setLanguages({
+      ...keywords,
+      newKeyword,
+    });
+  };
+
+  const onKeywordRemove = (keywords) => {
+    setKeywords(keywords);
+  };
+
+  const onLanguageRemove = (languages) => {
+    setLanguages(languages);
+  };
+  console.log(languages, keywords);
   return (
     <Page>
       <Admins>
-        <Typography variant="h2" colour="primary">Admins</Typography>
+        <Typography variant="h2" colour="primary">
+          Admins
+        </Typography>
       </Admins>
       <Domains>
-        <Typography variant="h2" colour="primary">Allowed email domain</Typography>
-        <Typography variant="p" colour="primary">{emailDomain}</Typography>
+        <Typography variant="h2" colour="primary">
+          Allowed email domain
+        </Typography>
+        <Typography variant="p" colour="primary">
+          {emailDomain}
+        </Typography>
       </Domains>
       <Languages>
-        <Typography variant="h2" colour="primary">Language proficiencies</Typography>
+        <Typography variant="h2" colour="primary">
+          Language proficiencies
+        </Typography>
+        {languages && (<ItemListEdit items={languages} onRemove={onLanguageRemove} />)}
+        <TextField
+          required
+          fullWidth
+          id="first_name_Edit"
+          type="text"
+          label="First name"
+          name="first_name"
+          value=""
+          onChange={(e) => {
+            onLanguageAdd(e.target.value);
+          }}
+        />
       </Languages>
       <Keywords>
-        <Typography variant="h2" colour="primary">Keywords</Typography>
+        <Typography variant="h2" colour="primary">
+          Keywords
+        </Typography>
+        {keywords && (<ItemListEdit items={keywords} onRemove={onKeywordRemove} />) }
+        <TextField
+          required
+          fullWidth
+          id="first_name_Edit"
+          type="text"
+          label="First name"
+          name="first_name"
+          value=""
+          onChange={(e) => {
+            onKeywordAdd(e.target.value);
+          }}
+        />
       </Keywords>
       <ActionButtonContainer>
         <Button fullWidth variant="contained" type="submit" color="primary">
