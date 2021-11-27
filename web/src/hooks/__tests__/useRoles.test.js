16b4b4feca3ea72994eff6d317e118a485760f5c
Hoang Tran <hoang.tran@tuni.fi>
2021-11-27 12:35:23 +0200

    feat: added test for hooks

---
+++ web/src/hooks/__tests__/useRoles.test.js
@@ -0,0 +1,35 @@
+import { renderHook } from "@testing-library/react-hooks";
+import useCompetences from "../useCompetences";
+
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+afterAll(() => {
+  Object.defineProperty(window.document, "cookie", {
+    writable: true,
+    value: "",
+  });
+});
+
+describe("useCompetenceCategories hook", () => {
+  it("should return an empty array when hub-jtw is not defined", () => {
+    const { result } = renderHook(() => useCompetences("cat"));
+    expect(result.current.length).toEqual(0);
+  });
+  it("should return categories list when hub-jwt is defined and the jwt is proper", async () => {
+    const mockCompetencesResponse = [
+      { id: 1, name: "cat", category: { id: 1 } },
+    ];
+    const mockCompetencesActual = [{ id: 1, name: "cat", category: 1 }];
+    fetch.mockResponseOnce(JSON.stringify(mockCompetencesResponse), {
+      status: 200,
+    });
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useCompetences("cat", "jwt")
+    );
+    await waitForNextUpdate();
+    expect(result.current).toEqual(mockCompetencesActual);
+  });
+});
