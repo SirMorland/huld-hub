4b11c4b95881cf1acd1a7e1ebbb1c70b876fdd78
Jori Väinölä <jori.vainola@tuni.fi>
2021-12-02 23:02:13 +0200

    fixed indentation in policies/canDelete

--- backend/app/api/user-profiles/config/policies/canDelete.js
+++ backend/app/api/user-profiles/config/policies/canDelete.js
@@ -1,15 +1,14 @@
 module.exports = async (ctx, next) => {
-    if (ctx.state.user) {
-      const { id } = ctx.params;
-      const { user } = ctx.state;
-      const [profile] = await strapi.services['user-profiles'].find({
-        id,
-        'user.id': user.id,
-      });
-      if (user.role.type === 'admin' && (!profile)) {
-        return await next();
-      } 
+  if (ctx.state.user) {
+    const { id } = ctx.params;
+    const { user } = ctx.state;
+    const [profile] = await strapi.services['user-profiles'].find({
+      id,
+      'user.id': user.id,
+    });
+    if (user.role.type === 'admin' && (!profile)) {
+      return await next();
     }
-    return ctx.unauthorized(`You're unauthorized to delete the profile`);
-  };
-  
+  }
+  return ctx.unauthorized(`You're unauthorized to delete the profile`);
+};
