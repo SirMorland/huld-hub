64c1f3469d9116eef342bc84f58bda5ce0825162
Hoang Tran <hoang.tran@tuni.fi>
2021-12-02 16:16:18 +0200

    fix: added async

--- backend/app/api/user-password/controllers/user-password.js
+++ backend/app/api/user-password/controllers/user-password.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  update: (ctx) => {
+   update: async (ctx) => {
     const { password } = ctx.request.body;
     ctx.request.body = { password: await strapi.admin.services.auth.hashPassword(password) };
     return strapi.plugins["users-permissions"].controllers.user.update(ctx);
